9ecb7aa3ccc21e63b1450e0d21b4d4a093a193ca
Syhenu <3138432008@qq.com>
2024-11-17 12:21:03 +0800

    添加index.js和vue.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import {createApp} from './vue.js'
+
+const app = createApp({
+    data() {
+        return{
+            name: "Syhenu"
+        }
+
+    },
+    mounted(){
+    },
+    methods:{
+    },
+    
+});
+app.mount('#app');
